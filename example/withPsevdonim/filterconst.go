c8c15a60c290a72d467580c5e95d82a7a73ec245
Илья Ипполитов <i.ippolitov@cloud.gcm>
2023-12-22 16:08:23 +0200

    ADD new test example with psewdoniv constant

---
+++ example/withPsevdonim/filterconst.go
@@ -0,0 +1,41 @@
+package withpsevdonim
+
+//go:generate stringer -type=FilterConst
+//go:generate enummethods -type=Status
+type FilterConst int
+
+const (
+	_lt = FilterConst(iota)
+	_lte
+	_gt
+	_gte
+	_ne
+	_not
+	_and
+	_or
+	_between
+	_like
+	_contains
+	_field
+	_value
+	_from
+	_to
+)
+
+const (
+	LT       = _lt
+	LTE      = _lte
+	GT       = _gt
+	GTE      = _gte
+	NE       = _ne
+	NOT      = _not
+	AND      = _and
+	OR       = _or
+	BETWEEN  = _between
+	LIKE     = _like
+	CONTAINS = _contains
+	FIELD    = _field
+	VALUE    = _value
+	FROM     = _from
+	TO       = _to
+)
