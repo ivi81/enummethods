45f593b166c3267f1797c63a77ea14407c7d0238
Илья Ипполитов <i.ippolitov@cloud.gcm>
2025-09-16 17:29:01 +0300

    RESTRUCTURE project

--- type.go
+++ type.go
@@ -2,13 +2,14 @@ package main
 
 // TypeInfo агрегирует информацию о типе данных необходимую для генерации методов данного типа
 // Поля:
-//   constNames - список названий констант
-//   withArray - используется массив или нет
-//   varName - название переменной содержащей строки
+//
+//	constNames - список названий констант
+//	withArray - используется массив или нет
+//	varName - название переменной содержащей строки
 type TypeInfo struct {
 	constNames ConstSlice
 	withArray  bool
-	pkgName string
+	pkgName    string
 }
 
 type ConstSlice []string
