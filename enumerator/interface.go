3573d90dd0c8022acdaf78f35137c4f24644ffe6
Илья Ипполитов <i.ippolitov@cloud.gcm>
2025-09-19 16:17:04 +0300

    FIX yamler interface for us with gopkg.in/yaml.v2

--- enumerator/interface.go
+++ enumerator/interface.go
@@ -9,32 +9,32 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-//Enumerator интерфейсный тип объявляющий функции для работы с нестроковыми константами
+// Enumerator интерфейсный тип объявляющий функции для работы с нестроковыми константами
 type Enumerator interface {
 	Stringer
 	Validator
 	Unstringer
 }
 
-//Stringer  интерфейсный тип объявляющий функции для преобразования
-//не строкового перечислимого типа в строку
+// Stringer  интерфейсный тип объявляющий функции для преобразования
+// не строкового перечислимого типа в строку
 type Stringer interface {
 	fmt.Stringer
 }
 
-//Unstringer  интерфейсный тип объявляющий функции для преобразования строки
-//не строкового перечислимого типа
+// Unstringer  интерфейсный тип объявляющий функции для преобразования строки
+// не строкового перечислимого типа
 type Unstringer interface {
 	SetValue(s string) bool
 }
 
-//Validator интерфейсный тип объявляющий функции для проверки допустимости значения
-//не строкового перечислимого типа
+// Validator интерфейсный тип объявляющий функции для проверки допустимости значения
+// не строкового перечислимого типа
 type Validator interface {
 	IsValid() bool
 }
 
-//MarshalJSON маршалинг значений не строковых перечислимых типов в формат json
+// MarshalJSON маршалинг значений не строковых перечислимых типов в формат json
 func MarshalJSON(c Stringer) ([]byte, error) {
 	var s string
 
@@ -50,7 +50,7 @@ func MarshalJSON(c Stringer) ([]byte, error) {
 	return json.Marshal(s)
 }
 
-//UnmarshalJSON анмаршалинг строкового значания поля в json-документе в значение не строкового константного типа
+// UnmarshalJSON анмаршалинг строкового значания поля в json-документе в значение не строкового константного типа
 func UnmarshalJSON(c Unstringer, data []byte) error {
 	var (
 		s   string
@@ -70,26 +70,27 @@ func UnmarshalJSON(c Unstringer, data []byte) error {
 	return err
 }
 
-//MarshalYAML маршалинг значений не строковых перечислимых типов в формат yaml
-func MarshalYAML(c Stringer) ([]byte, error) {
+// MarshalYAML маршалинг значений не строковых перечислимых типов в формат yaml
+func MarshalYAML(c Stringer) (interface{}, error) {
 	var s string
 
 	if s = c.String(); s == "" {
 		return nil, errors.New(fmt.Sprintf("value:\"%b\" is not convertable to string", c))
 	}
-
-	return yaml.Marshal(s)
+	return s, nil
+	//return yaml.Marshal(s)
 }
 
-//UnmarshalYAML анмаршалинг строки в поле yaml-документа в значение не строкового константного типа
-func UnmarshalYAML(c Unstringer, data []byte) error {
+// UnmarshalYAML анмаршалинг строки в поле yaml-документа в значение не строкового константного типа
+func UnmarshalYAML(c Unstringer, unmarshal func(interface{}) error) error {
 	var (
 		s   string
 		err error
 	)
-	if err = yaml.Unmarshal(data, &s); err != nil {
+	if err = unmarshal(&s); err != nil {
 		return err
 	}
+
 	if !c.SetValue(s) {
 
 		err = &yaml.TypeError{
--- tmpl/methods/yamler.go
+++ tmpl/methods/yamler.go
@@ -1,20 +1,21 @@
-//yamler.go - содержит шаблон для генерации методов рерализующих интерфейсы json.Marshaler, json.UnMarshaler
+// yamler.go - содержит шаблон для генерации методов рерализующих интерфейсы json.Marshaler, json.UnMarshaler
 package methods
 
 import "text/template"
 
-//YamlerTemplate - переменная содержит шаблон на основе которого генерируются методы рерализующие интерфейсы json.Marshaler, json.UnMarshaler
-//  Параметры шаблона:
-//   -.TypeName — название типа данных для которого генерируются методы,
+// YamlerTemplate - переменная содержит шаблон на основе которого генерируются методы рерализующие интерфейсы json.Marshaler, json.UnMarshaler
+//
+//	Параметры шаблона:
+//	 -.TypeName — название типа данных для которого генерируются методы,
 var YamlerTmpl = template.Must(template.New("").Parse(`
 
 //MarshalYAML - реализует метод интерфейса yaml.Marshaler
-func (m {{.TypeName}}) MarshalYAML() ([]byte, error) {
+func (m {{.TypeName}}) MarshalYAML() (interface{}, error) {
 	return enumerator.MarshalYAML(m)
 }
 
 //UnmarshalYAML - реализует метод интерфейса yaml.UnMarshaler
-func (m *{{.TypeName}}) UnmarshalYAML(data []byte) error {
-	return enumerator.UnmarshalYAML(m, data)
+func (m *{{.TypeName}}) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	return enumerator.UnmarshalYAML(m, unmarshal)
 }
 `))
